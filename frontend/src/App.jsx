9a5c4d6579c4157d47e28dff76042eda660cd3ad
Vili Auvinen <2207441@edu.karelia.fi>
2026-01-10 08:48:58 +0200

    vite react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Yatchee Multiplayer</h1>
+      <p>React frontend is running.</p>
+    </div>
+  );
+}
+
+export default App;
